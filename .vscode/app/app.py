8d3c4dcec2831974c09c794effd0b7b7d80cd1c7
Hugo B <157296697+HugoBrandaoo@users.noreply.github.com>
2024-02-14 13:15:41 -0300

    Update calculadora
    Para essa update, consegui fazer os comandos funcionarem. Código feito com apoio a um vídeo do youtube, porem consegui aprender bastante e compreender com clareza como funciona o codigo.

--- .vscode/app/app.py
+++ .vscode/app/app.py
@@ -8,33 +8,29 @@ def main(page: ft.Page):
     page.title = 'Calculadora'
 
     def get_data(e):
-        resultado_at = resultado.value if resultado.value != '0' else ''
         data = e.control.data
-        if data in ('0','1','2','3','4','5','6','7','8','9'):
-            data = resultado_at + data
-        
-        else:
-            if resultado_at and resultado_at[:-1] in ('+','-','*','/','.'):
-                data = resultado_at[:-1]
-        
-            if data[-1] in ('=','%','+/-'):
-                data = calculo(data = data[-1], resultado_at = resultado_at)
-            
-        resultado.value = data
+        if data == '=':
+            try:
+                resultado.value = str(eval(resultado.value))
+            except Exception as e:
+                resultado.value = 'ERROR'
+        elif data == 'C':
+            resultado.value = ''
+        elif data == '%':
+            try:
+                resultado.value = str(float(resultado.value)/100)
+            except ValueError:
+                resultado.value = 'ERROR'
+        elif data == '.':
+            if not resultado.value or resultado.value[:-1] not in ['.','+','-','*','/']:
+                resultado.value += data
+        elif data == '+/-':
+            resultado.value = '-' + resultado.value if resultado.value and resultado.value[0] != '-' else resultado.value[:-1]
+        elif data in ['1','2','3','4','5','6','7','8','9','0','.','+','-','*','/']:
+            resultado.value += data
+                      
+
         page.update()
-
-    def clear(e):
-        resultado.value = ''
-        page.update()
-    
-    def calculo(data, resultado_at):
-        data = eval(resultado_at)
-        return data
-
-    page.add(
-        
-    )
-
     resultado = ft.Text(value= '0', color= 'White')
     page.add(
         ft.Row(controls=[
@@ -43,7 +39,7 @@ def main(page: ft.Page):
     )
     page.add(
         ft.Row(controls=[
-            ft.ElevatedButton(text= 'AC',bgcolor= ft.colors.TRANSPARENT,color= 'Red', expand= 1, style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()),on_click= clear),
+            ft.ElevatedButton(text= 'C',bgcolor= ft.colors.TRANSPARENT,color= 'Red', expand= 1,data = 'C', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()),on_click= get_data),
             ft.ElevatedButton(text= '+/-',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '+/-', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
             ft.ElevatedButton(text= '%',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '%', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
             ft.ElevatedButton(text= '/',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '/', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
@@ -51,33 +47,33 @@ def main(page: ft.Page):
     )
     page.add(
         ft.Row(controls=[
-            ft.ElevatedButton(text= ' 7 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '7', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' 8 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '8', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' 9 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '9', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' * ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '*', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '7',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '7', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '8',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '8', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '9',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '9', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '*',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '*', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
         ])
     )
     page.add(
         ft.Row(controls=[
-            ft.ElevatedButton(text= ' 4 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '4', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' 5 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '5', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' 6 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '6', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' - ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '-', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '4',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '4', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '5',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '5', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '6',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '6', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '-',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '-', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
         ])
     )
     page.add(
         ft.Row(controls=[
-            ft.ElevatedButton(text= ' 1 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '1', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' 2 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '2', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' 3 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '3', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' + ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '+', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '1',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '1', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '2',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '2', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '3',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '3', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '+',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '+', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
         ])
     )
     page.add(
         ft.Row(controls=[
-            ft.ElevatedButton(text= ' 0 ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '0', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()),on_click= get_data),
-            ft.ElevatedButton(text= ' . ',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '.', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
-            ft.ElevatedButton(text= ' = ',bgcolor= ft.colors.TRANSPARENT,color= 'Green', expand= 1,data = '=', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '0',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '0', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()),on_click= get_data),
+            ft.ElevatedButton(text= '.',bgcolor= ft.colors.TRANSPARENT, color= 'White', expand= 1,data = '.', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
+            ft.ElevatedButton(text= '=',bgcolor= ft.colors.TRANSPARENT,color= 'Green', expand= 1,data = '=', style= ft.ButtonStyle(padding= 25, shape=ft.CircleBorder()), on_click= get_data),
         ])
     )
 ft.app(target=main)
